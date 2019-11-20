71bfe3e5fa3ffd3dd66d786be166a8a325c70c60
Raymond <yuraymond96@gmail.com>
2019-11-20 14:27:58 -0800

    Dump commit
    BT working

---
+++ Assets/Scripts/LetsGetBluetooth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TechTweaking.Bluetooth;
+
+public class LetsGetBluetooth : MonoBehaviour {
+
+    private BluetoothDevice device;
+    public Text statusText;
+    public Text sizeOfMessage; // get message size
+    public Text messageZero; // message at index 0
+    public Text messageOne; // message at index 1
+    public Text messageTwo; // message at index 2
+
+    void Awake()
+    {
+        BluetoothAdapter.askEnableBluetooth();
+
+        device = new BluetoothDevice();
+        device.Name = "ESP32test";
+    }
+
+    public void connect()
+    {
+        statusText.text = "STATUS: CONNECTING...";
+
+        device.connect();
+    }
+
+    public void disconnect()
+    {
+        device.close();
+    }
+
+    // Use this for initialization
+    void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
---
+++ Resources/COM.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System;
+using System.Threading;
+
+public class COM : MonoBehaviour {
+	
+	public SerialPort sp;
+	public int bytes;
+	public Thread serialThread;
+	public float x1,y1,z1;
+	public bool readyToMove = false;
+	public float prevY = 0.0f;
+
+	void Start () {
+		sp = new SerialPort("COM3", 19200);
+	}
+
+	void parseValues(string av) {
+
+		string[] split = av.Split (',');
+	    x1 = float.Parse (split [2]);
+		y1 = float.Parse (split [3]);
+		z1 = float.Parse(split[4]);
+		readyToMove = true;
+	}
+
+
+	void moveObj(float x, float y, float z) {
+		int speed = 10;
+		Vector3 move = Vector3.zero ;
+		move.y = y;
+		move.Normalize();
+		move.y = Mathf.Lerp(move.y,prevY,speed*Time.deltaTime);
+		transform.Translate(Vector3.up * (move.y * speed) * Time.deltaTime,Space.World);
+		readyToMove = false;
+		prevY = move.y;
+	}
+
+	
+	 void recData() {
+		if ((sp != null) && (sp.IsOpen)) {
+			byte tmp;
+			string data = "";
+			string avalues="";
+			tmp = (byte) sp.ReadByte();
+			while(tmp !=255) {
+				data+=((char)tmp);
+				tmp = (byte) sp.ReadByte();
+				if((tmp=='>') && (data.Length > 30)){
+					avalues = data;
+					parseValues(avalues);
+					data="";
+				}
+			}
+		}
+	}
+
+
+	void connect() {
+		Debug.Log ("Connection started");
+		try {
+			sp.Open();
+			sp.ReadTimeout = 400;
+			sp.Handshake = Handshake.None;
+			serialThread = new Thread(recData);
+			serialThread.Start ();
+			Debug.Log("Port Opened!");
+		}catch (SystemException e)
+		{
+			Debug.Log ("Error opening = "+e.Message);
+		}
+	
+	}
+
+
+	void Update () { 
+
+		 if (Input.GetKeyDown ("x")) {
+			Debug.Log("Connection establishing...");
+			connect ();
+		}
+
+		if (readyToMove == true) {
+			moveObj(x1,y1,z1);
+		}
+
+	}
+
+
+}
