87401e136c7d0fa0d6e226554ff4e23495fb95e2
Raymond <yuraymond96@gmail.com>
2019-11-24 22:13:22 -0800

    Create SystemScripts.cs
    Screen orientation and program quit

---
+++ Assets/Scripts/SystemScripts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+	
+    public void exitApp ()
+    {
+        Application.Quit();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
