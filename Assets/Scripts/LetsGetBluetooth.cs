73350fb99465b77e8d0083d448e13b33513c0e31
Raymond <yuraymond96@gmail.com>
2019-11-24 21:23:05 -0800

    Updated Letsgetbluetooth fixed text drag issue
    Non verified

--- Assets/Scripts/LetsGetBluetooth.cs
+++ Assets/Scripts/LetsGetBluetooth.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 using TechTweaking.Bluetooth;
+using TMPro;
 
 public class LetsGetBluetooth : MonoBehaviour {
 
     private BluetoothDevice device;
-    public Text statusText;
-    public Text sizeOfMessage; // get message size
-    public Text messageZero; // message at index 0
-    public Text messageOne; // message at index 1
-    public Text messageTwo; // message at index 2
+    public TextMeshProUGUI statusText;
+    public TextMeshProUGUI sizeOfMessage; // get message size
+    public TextMeshProUGUI messageZero; // message at index 0
+    public TextMeshProUGUI messageOne; // message at index 1
+    public TextMeshProUGUI messageTwo; // message at index 2
+    public TextMeshProUGUI messageThree; // message at index 3
+    public TextMeshProUGUI messageFour; // message at index 4
 
     void Awake()
     {
@@ -34,13 +37,29 @@ public class LetsGetBluetooth : MonoBehaviour {
         device.close();
     }
 
-    // Use this for initialization
-    void Start() {
-		
-	}
+    public void startTransmission()
+    {
+        if (device != null)
+            device.send(System.Text.Encoding.ASCII.GetBytes("Start\n"));
+    }
 	
 	// Update is called once per frame
 	void Update() {
-		
+		if (device.IsReading)
+        {
+            byte[] msg = device.read();
+
+            if (msg != null)
+            {
+                statusText.text = "MSG RECEIVED: " + msg[0].ToString() + msg[1].ToString() + msg[2].ToString();
+                //statusText.text = "MSG RECEIVED: " + foreach (byte item in msg) {item.ToString()};
+                sizeOfMessage.text = "MSG SIZE: " + msg.Length;
+                messageZero.text = msg[0].ToString();
+                messageOne.text = msg[1].ToString();
+                messageTwo.text = msg[2].ToString();
+                //messageThree.text = msg[3].ToString();
+                //messageFour.text = msg[4].ToString();
+            }
+        }
 	}
 }
