7dd30893cdf2e580b3cf4024dc611e4a06e277b5
Raymond <yuraymond96@gmail.com>
2019-11-28 17:54:28 -0800

    Added location services GPS

--- Assets/Scripts/SystemScripts.cs
+++ Assets/Scripts/SystemScripts.cs
@@ -1,21 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SystemScripts : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public TextMeshProUGUI longitude;
+    public TextMeshProUGUI latitude;
+
+    IEnumerator Start ()
+    {
         Screen.orientation = ScreenOrientation.LandscapeRight;
+
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        Input.location.Start();
+
+        int maxWait = 20; // wait until service initializes
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait < 1)
+        {
+            Debug.Log("GNSS timed out...");
+            yield break;
+        }
+        else
+        {
+            Debug.Log("Location service functioning!");
+            longitude.text = Input.location.lastData.longitude.ToString();
+            latitude.text = Input.location.lastData.latitude.ToString();
+        }
     }
 	
     public void exitApp ()
     {
+        Input.location.Stop();
         Application.Quit();
     }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
